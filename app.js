826306326e52037311d3439124f7e92256fcea03
Michael <mju@berkeley.edu>
2015-12-31 19:21:39 -0800

    Add socket.io capability and connect to MongoDB

--- app.js
+++ app.js
@@ -1,24 +1,75 @@
 var express = require('express');
 var http = require('http');
 var path = require('path');
+var bodyParser = require('body-parser');
 var app = express();
 var server = http.createServer(app);
+var io = require('socket.io')(server);
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error'));
+db.once('open', function() {
+	console.log('database connected');
+	var Schema = mongoose.Schema;
+	var userSchema = new Schema({
+		firstname: String,
+		lastname: String,
+		username: String,
+		password: String,
+		email: String
+	});
+	var User = mongoose.model('User', userSchema);
+});
+
+
+io.on('connection', function(socket) {
+	socket.on('message', function(message) {
+		//TODO: emit message to only people in the chat room from which you got the message
+		io.emit('message', message);
+	});
+});
 
 
 // TODO: move routes to router modules
 app.get('/', function(req, res, next) {
 	res.render('index');
 });
+app.route('/signup')
+	.get(function(req, res, next) {
+		res.render('signup');
+	})
+	.post(function(req, res, next) {
+		var account_info = req.body;
+		for (var key in account_info) {
+			if (account_info.hasOwnProperty(key)) {
+				if (account_info[key] == '') {
+					res.render('signup', {error: 'One or more fields were left blank.'});
+					return; //NEED this return
+				}
+			}
+		}
+		res.redirect('/contactdir');
+	});
+
+
 // TODO: chat room directory page
 // TODO: private chat room feature
 // TODO: chat log feature; only store 50 or so messages in the browser DOM for performance
+// TODO: new joiner of chat room can load previous messages
 app.get('/chatroom', function(req, res, next) {
 	res.render('chatroom');
+});
+app.get('/contactdir', function(req, res, next) {
+	res.render('contactdir');
 });
 
 
